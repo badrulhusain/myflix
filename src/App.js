c81b45d46df7ef61f87958fcac3b8a1c1a3186c4
C_Alchemy <136973340+badrulhusain@users.noreply.github.com>
2024-06-21 13:51:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+import axios from 'axios'
+import './app.css'
+import {useState} from'react'
+
+function App() {
+  const [state,setState]=useState([])
+  return (
+    <div className='App'>
+      <h1>hello world</h1>
+    <button onClick={()=>{
+      axios.get('https://jsonplaceholder.typicode.com/posts').then((res)=>{
+        
+        console.log(res.data);
+        setState(res.data);
+
+      })
+    }}> Click me </button>
+    {state.map((obj,index)=>{
+      return(
+        <div>
+          <h1>{index+1}</h1>
+          <h1>{obj.title}</h1>
+          <h4>{obj.body}</h4>
+        </div>
+      )
+    })}
+    </div>
+  )
+}
+
+export default App
